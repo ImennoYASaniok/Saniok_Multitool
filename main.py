17a0c0d16ba9b4024121107b06bae08cddb742b7
Saniok <banlichelendsh@yandex.ru>
2025-09-15 23:15:06 +0300

    kostil async web port

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from aiohttp import web
+
 import asyncio
 import os
 
@@ -16,9 +18,20 @@ async def start_bot():
     await set_commands()
     await dp.start_polling(bot)
 
+async def handle(request):
+    return web.Response(text="Bot is running!")
+
+app = web.Application()
+app.router.add_get("/", handle)
+
 if __name__ == "__main__":
     db_file = os.path.join(
         os.path.abspath(os.path.dirname(__file__)), 'db', 'db.sqlite'
     )
     db_session.my_global_init(os.environ.get('DATABASE_URI', db_file))
-    asyncio.run(start_bot())
+
+    port = int(os.environ.get("PORT", 10000))
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(web._run_app(app, port=port, print=None))
+    loop.run_until_complete(start_bot())
