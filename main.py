fefc0c05b2305d78e2731ef3ee02a178dc6ed7fa
Saniok <banlichelendsh@yandex.ru>
2025-09-16 00:06:43 +0300

    extra logger

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from aiohttp import web
 
 import asyncio
@@ -9,6 +10,20 @@ from handlers.handler_send_message import send_message_router
 from bot_session import dp, bot, set_commands
 from db import db_session
 
+LOG_FILE = "server.log"
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(LOG_FILE, encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
+
+def handle_async_exception(loop, context):
+    msg = context.get("exception", context["message"])
+    logger.exception(f"Необработанное исключение в asyncio: {msg}")
 
 async def start_bot():
     dp.include_router(menu_router)
@@ -16,13 +31,13 @@ async def start_bot():
     dp.include_router(send_message_router)
     await bot.delete_webhook(drop_pending_updates=True)
     await set_commands()
-    await dp.start_polling(bot)
+    try:
+        await dp.start_polling(bot, skip_updates=True)
+    except Exception as e:
+        print("Ошибка бота:", e)
 
 async def handle(request):
     return web.Response(text="Bot is running!")
-
-app = web.Application()
-app.router.add_get("/", handle)
 
 if __name__ == "__main__":
     db_file = os.path.join(
@@ -31,7 +46,14 @@ if __name__ == "__main__":
     db_session.my_global_init(os.environ.get('DATABASE_URI', db_file))
 
     port = int(os.environ.get("PORT", 10000))
+    app = web.Application()
+    app.router.add_get("/", handle)
 
     loop = asyncio.get_event_loop()
+    loop.set_exception_handler(handle_async_exception)
     loop.create_task(web._run_app(app, port=port, print=None))
     loop.run_until_complete(start_bot())
+    try:
+        loop.run_until_complete(start_bot())
+    except Exception:
+        logger.exception("Главный цикл сервера упал:")
